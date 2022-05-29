7cacccabb4239ad94427ce5b48d1355a4bbf88a0
Sebastian Falana <s.falana@azagroup.eu>
2022-05-29 14:56:44 +0200

    add new pages

---
+++ src/main/java/pages/commons/TopManuPage.java
@@ -0,0 +1,10 @@
+package pages.commons;
+
+import org.openqa.selenium.WebDriver;
+import pages.base.BasePage;
+
+public class TopManuPage extends BasePage {
+    public TopManuPage(WebDriver driver) {
+        super(driver);
+    }
+}
